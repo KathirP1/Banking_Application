6e32719d0661e2b8e08cb0bf61b0ea773c2f93e0
Kathir P <130573492+KathirP1@users.noreply.github.com>
2024-06-25 19:35:48 +0530

    Add files via upload

--- banking-app/src/main/java/com/project/banking/entity/Account.java
+++ banking-app/src/main/java/com/project/banking/entity/Account.java
@@ -3,9 +3,10 @@ package com.project.banking.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+@NoArgsConstructor
 @Data
-@Entity
 @Table(name = "accounts")
+@Entity
 public class Account {
 
     @Id
@@ -15,6 +16,40 @@ public class Account {
     @Column(name = "account_holder_name")
     private String accountHolderName;
     private double balance;
+
+    public Account(Long id, String accountHolderName, double balance) {
+        this.id=id;
+        this.accountHolderName=accountHolderName;
+        this.balance=balance;
+    }
+
+
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
 
 
 }
--- banking-app/src/main/java/com/project/banking/entity/Transaction.java
+++ banking-app/src/main/java/com/project/banking/entity/Transaction.java
@@ -17,4 +17,44 @@ public class Transaction {
     private double amount;
     private String transactionType; // DEPOSIT, WITHDRAW, TRANSFER
     private LocalDateTime timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
